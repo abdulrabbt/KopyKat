c654931ed4ebe7152b04916dcbadd413e96b367c
moralesl5 <loganjmorales@gmail.com>
2017-06-07 11:08:39 -0400

    Linked paged, added base content.

---
+++ js/main.js
@@ -0,0 +1,15 @@
+console.log("JS Linked!");
+
+// Start of jQuery
+jQuery(function() {
+	console.log("JQ Linked!");
+
+	var gameBoard1 = [
+        ["", ""],
+        ["", ""]
+    ];
+
+    
+
+
+});
