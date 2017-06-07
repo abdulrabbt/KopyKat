68599dd63119d7c05214bba0c62f8e149122f0ac
moralesl5 <loganjmorales@gmail.com>
2017-06-07 11:47:13 -0400

    Used jquery to append a board to lvl 1, colored the boxes

--- js/main.js
+++ js/main.js
@@ -2,12 +2,36 @@ console.log("JS Linked!");
 
 // Start of jQuery
 jQuery(function() {
+
 	console.log("JQ Linked!");
 
-	var gameBoard1 = [
+	var lvlOneBoard = $('#boardOne');
+	console.log(lvlOneBoard);
+	var counter = 1;
+
+	var logicBoardOne = [
         ["", ""],
         ["", ""]
     ];
+
+
+    for (var i = 0; i < logicBoardOne.length; i++){
+    	var lvlOneRow = $("<div>");
+    	lvlOneRow.addClass("row");
+    	lvlOneBoard.append(lvlOneRow);
+    	for (var j = 0; j < logicBoardOne[i].length; j++){
+    		var lvlOneCell = $("<div>");
+    		lvlOneCell.addClass("cell");
+    		lvlOneCell.attr("id", "box" + counter);
+    		lvlOneBoard.append(lvlOneCell)
+
+
+
+    		counter++;
+
+    	};
+
+    };
 
     
 
