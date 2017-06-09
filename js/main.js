3208a0be8fcf685ce56bf022e6d1fa4d7fa44083
moralesl5 <loganjmorales@gmail.com>
2017-06-09 12:46:28 -0400

    Logic progress, sorta...

--- js/main.js
+++ js/main.js
@@ -3,15 +3,91 @@ console.log("JS Linked!");
 // Start of jQuery
 jQuery(function() {
 
-    console.log("JQ Linked!");
 
-    var lvlOneBoard = $('#boardOne');
-    var counter = 1;
+            
 
-    var logicInit = Math.ceil(Math.random() * 4);
-    console.log(logicInit);
-    var logicSeq = [logicInit]; // Logic sequence
-    var userSeq = []; // User sequence
+
+            var logicArray = [];
+            var userArray = [];
+
+            var cells = $(".cell");
+            console.log(cells);
+
+            var fillLogic = function() {
+                for (var i = 0; i < 10; i++) {
+                    var randomNumber = Math.floor(Math.random() * 4);
+                    logicArray.push(randomNumber);
+                }
+            }
+            fillLogic();
+            console.log(logicArray);
+
+            
+            var turn = logicArray.length;
+            var c = 0;
+            var flashSeq = setInterval(function(){
+                console.log('this is c: ' + c)
+                $('.active').animate({
+                    opacity: '1'
+                }, 1000);
+                $('.active').removeClass("active");
+                // console.log("Class active removed!!!!!")
+                $('.cell').eq(logicArray[c]).addClass('active');
+                $(".active").animate({
+                    opacity: '0'
+                }, 1000)
+                // console.log("Class active added!!!");
+                // console.log(logicArray[c]);
+                if (c === turn){
+                    clearInterval(flashSeq)
+                   
+                };
+                c++;
+
+            }, 1000);
+
+            // var flashOff = function(el1) {
+            //     console.log('flashing off for ' + el1)
+            //     $(el1).removeClass('toggle');
+            // }
+
+            // var flashOn = function(el1) {
+            //     console.log('flashing on for ' + el1)
+            //     $(el1).addClass('toggle');
+            //     setTimeout(function() {
+            //         flashOff(el1);
+            //     }, 500)
+            // }
+
+            
+
+
+            // var flashSequence = function() {
+            //     // for (var i = 0; i < logicArray.length; i++) {
+            //         // debugger
+            //         var currentIndex = i;
+            //         var currentValue = "#" + logicArray[i];
+            //         setTimeout(function() {
+            //             flashOn($(currentValue));
+            //             console.log($(currentValue));
+            //         },500 );
+            //     // };
+            // };
+
+            
+            // flashSequence();
+
+
+
+    // console.log("JQ Linked!");
+
+    // var lvlOneBoard = $('#boardOne');
+    // var counter = 1;
+
+    // var logicInit = Math.ceil(Math.random() * 4);
+    // console.log(logicInit);
+    // var logicSeq = [logicInit]; // Logic sequence
+    // var userSeq = []; // User sequence
 
     // var dataBase = {    // WORK ON THIS IDEA
     //  logSeq:[1],
@@ -23,70 +99,76 @@ jQuery(function() {
 
 
 
-    var logicBoardOne = [
-        ["", ""],
-        ["", ""]
-    ];
+    // var logicBoardOne = [
+    //     ["", ""],
+    //     ["", ""]
+    // ];
 
-    function flashNewCell() {
-        for (var i = 0; i < logicSeq.length; i++) {
-            $("#" + logicSeq[i]).addClass("toggle");
-            window.setTimeout(() => {
-                console.log('this insideofOnClick -> ', $(this));
-                $("#" + logicSeq[0]).removeClass("toggle");
-                console.log('removed toggle');
-                console.log($(this));
-            }, 400);
+    // function flashSequence() {
+    //     console.log('flashing cell!');
+    //     for (var i = 0; i < logicSeq.length; i++) {
+    //         console.log('adding toggle');
+    //         console.log('!!!!' + i);
+    //         $("#" + logicSeq[i]).addClass("toggle");
+    //         var currentIndex = i;
+    //         window.setTimeout(function(){
+    //             // console.log('this insideofOnClick -> ', $(this));
+    //             console.log('removing toggle ' + i);
+    //             console.log(logicSeq);
+    //             $("#" + logicSeq[i]).removeClass("toggle");
+    //             // console.log($(this));
+    //         }, 400);
 
-        }
+    //     }
 
-    };
-
-
+    // };
 
 
-    function onCellClick(event) {
-        logicSeq.push(Math.ceil(Math.random() * 4));
-        console.log("Logic: " + logicSeq); // LOG CURRENT LOGIC ARRAY
-        userSeq.push(event.target.id);
-        console.log("User: " + userSeq); // LOG CURRENT USER ARRAY
-        // console.table(logicBoardOne); // LOG TABLE
-        // console.log("clicked!"); // LOG A CLICK
-        $(this).addClass("toggle"); // not happening, nvm it is...
-        let that = $(this);
-        window.setTimeout(() => {
-            console.log('this insideofOnClick -> ', $(this));
-            $(this).removeClass("toggle");
-            console.log('removed toggle');
-            console.log($(this));
-            flashNewCell();
-        }, 400);
-        
 
-    }
 
-    //fucntion setBoard
-    for (var i = 0; i < logicBoardOne.length; i++) {
-        var lvlOneRow = $("<div>");
-        lvlOneRow.addClass("row");
-        lvlOneBoard.append(lvlOneRow);
-        for (var j = 0; j < logicBoardOne[i].length; j++) {
-            // var red = Math.floor(Math.random() * 255);
-            // var green = Math.floor(Math.random() * 255);
-            // var blue = Math.floor(Math.random() * 255);
+    // function onCellClick(event) {
+    //     logicSeq.push(Math.ceil(Math.random() * 4));
+    //     console.log('push next part to sequence');
+    //     console.log("Logic: " + logicSeq); // LOG CURRENT LOGIC ARRAY
+    //     userSeq.push(event.target.id);
+    //     console.log("User: " + userSeq); // LOG CURRENT USER ARRAY
+    //     // console.table(logicBoardOne); // LOG TABLE
+    //     // console.log("clicked!"); // LOG A CLICK
+    //     $(this).addClass("toggle"); // not happening, nvm it is...
+    //     let that = $(this);
+    //     window.setTimeout(function(){
+    //         console.log('that insideofOnClick -> ', that);
+    //         that.removeClass("toggle");
+    //         console.log('removed toggle');
+    //         flashSequence();
+    //     }, 400);
+         
 
-            var lvlOneCell = $("<div>");
-            lvlOneCell.addClass("cell");
-            // lvlOneCell.css("background-color", 'rgb(' + red + ", " + green + ", " + blue + ")" ); // rgb(255, 0, 0)
-            lvlOneCell.attr("id", +counter);
-            flashNewCell();
-            lvlOneCell.click(onCellClick);
-            lvlOneBoard.append(lvlOneCell);
-            counter++;
+    // }
 
-        };
+    // //fucntion setBoard
+    // for (var i = 0; i < logicBoardOne.length; i++) {
+    //     var lvlOneRow = $("<div>");
+    //     lvlOneRow.addClass("row");
+    //     lvlOneBoard.append(lvlOneRow);
+    //     for (var j = 0; j < logicBoardOne[i].length; j++) {
+    //         // var red = Math.floor(Math.random() * 255);
+    //         // var green = Math.floor(Math.random() * 255);
+    //         // var blue = Math.floor(Math.random() * 255);
 
-    };
+    //         var lvlOneCell = $("<div>");
+    //         lvlOneCell.addClass("cell");
+    //         // lvlOneCell.css("background-color", 'rgb(' + red + ", " + green + ", " + blue + ")" ); // rgb(255, 0, 0)
+    //         lvlOneCell.attr("id", +counter);
+    //         lvlOneCell.click(onCellClick);
+    //         lvlOneBoard.append(lvlOneCell);
+    //         counter++;
+
+    //     };
+
+    // };
+
+    // flashSequence();
 
 
 
