d3c94f5612cd5d4e5f23af21620a446ecb06d5c1
moralesl5 <loganjmorales@gmail.com>
2017-06-12 13:52:57 -0400

    Added different difficulties, added scores, added highscores

--- js/main.js
+++ js/main.js
@@ -7,115 +7,368 @@ jQuery(function() {
     var logicArray = [Math.floor(Math.random() * 4)];
     var userArray = [];
 
-    var currentTurn = 1;
+    var score = 0;
 
     var cells = $(".cell");
     // console.log(cells);
 
-    // Populate logic array with a random sequence
-    var fillLogic = function() {
-        var randomNumber = Math.floor(Math.random() * 4);
-        logicArray.push(randomNumber);
-    }
 
-    // fillLogic();
+    // START OF EASY MODE
+    $("#easy").one("click", function() {
+            $("#medium").hide();
+            $("#hard").hide();
 
-
-    // Function to flash a cell back on
-    var flashOn = function(el1) {
-        $(el1).animate({
-            opacity: '0.3'
-        }, 100);
-    }
-
-    var wrongAnswer = function() {
-        document.getElementById("wrongChime").play(); // play wrong chime
-        console.log("Chime: wrong");
-        $("body").css("background-color", "white");
-        window.setTimeout(function() {
-            $("body").css("background-color", "black");
-        }, 100);
-    }
+            var startEasy = function() {}
+                // Populate logic array with a random sequence
+            var fillLogic = function() {
+                var randomNumber = Math.floor(Math.random() * 4);
+                logicArray.push(randomNumber);
+            }
 
 
-    var checkSeq = function() {
-        console.log('checking sequence')
-        var checkCounter = 0;
-        console.log('logic array is ' + logicArray);
-        console.log('user array is: ' + userArray)
-        for (var i = 0; i < logicArray.length; i++) {
-            if (userArray[i] !== logicArray[i]) {
-                console.log('WRONG')
-                return wrongAnswer();
-                // alert("they dont match!");
-            } else {
+            // Function to flash a cell back on
+            var flashOn = function(el1) {
+                $(el1).animate({
+                    opacity: '0.3'
+                }, 100);
+            }
+
+            // Check for wrong answer
+            var wrongAnswer = function() {
+                document.getElementById("wrongChime").play(); // play wrong chime
+                console.log("Chime: wrong");
+                $("body").css("background-color", "white");
+                window.setTimeout(function() {
+                    $("body").css("background-color", "black");
+                }, 100);
+            }
+
+            // SHOW CURRENT SCORE
+            var showScore = function() {
+                score++;
+                console.log("Score: " + score);
+                $(".scoreNumber").empty();
+                $(".scoreNumber").append(score)
+            }
+
+            // SHOW HIGH SCORE 
+            var showHighScore = function() {
+                var highScore = 0;
+                if (score > highScore) {
+                    highScore = score;
+                }
+                $(".highScoreNumber").empty();
+                $(".highScoreNumber").append(highScore);
 
             }
-        }
-        console.error('resetting userArray')
-        document.getElementById("rightChime").play(); // PLAY CORRECT CHIME!
-        userArray = [];
-        fillLogic();
-        displaySequenceToTurnCount();
-        // console.log("logic array: " + logicArray);
-
-    }
 
 
+            var checkSeq = function() {
+                console.log('checking sequence')
+                var checkCounter = 0;
+                console.log('logic array is ' + logicArray);
+                console.log('user array is: ' + userArray)
+                for (var i = 0; i < logicArray.length; i++) {
+                    if (userArray[i] !== logicArray[i]) {
+                        console.log('WRONG')
+                        return wrongAnswer();
+                    } else {
 
-    // Flash cell on user click
-
-    var userClick = function() {
-        var activeCell = $("#" + event.target.id);
-        // console.log(activeCell);
-        activeCell.animate({ // animate click
-            opacity: '1'
-        }, 100, function() { setTimeout(flashOn(activeCell)), 50 });
-        userArray.push(parseInt(event.target.id)); 
-
-
-
-
-        if (userArray.length == logicArray.length) {
-            console.log('array lengths match')
-
-            checkSeq();
-
-
-            // console.log("Chime: correct");
-            // console.log("Clicked: " + event.target.id);
-            // console.log("User array: " + userArray);
-
-        } 
+                    }
+                }
+                console.error('resetting userArray')
+                document.getElementById("rightChime").play(); // PLAY CORRECT CHIME!
+                userArray = [];
+                fillLogic();
+                displaySequenceToTurnCount();
+                showScore();
+                showHighScore();
 
 
-    };
 
-    
+            }
 
 
-    function displaySequenceToTurnCount() {
-        // console.log('logic array', logicArray);
-        var c = 0;
-        // timeout
-        var flashSeq = setInterval(function() {
-            var activeCell = $('.cell').eq(logicArray[c]) // returns element with index
-            activeCell.animate({
-                opacity: '1'
-            }, 100, function() { setTimeout(flashOn(activeCell)), 10 });
-            c++;
-            if (c == logicArray.length) {
-                clearInterval(flashSeq);
+
+
+            // Flash cell on user click
+
+            var userClick = function() {
+                var activeCell = $("#" + event.target.id);
+                // console.log(activeCell);
+                activeCell.animate({ // animate click
+                    opacity: '1'
+                }, 100, function() { setTimeout(flashOn(activeCell)), 50 });
+                userArray.push(parseInt(event.target.id));
+
+                if (userArray.length == logicArray.length) {
+                    console.log('array lengths match');
+                    checkSeq();
+                }
             };
 
 
-        }, 500);
-        // How long before the next flash, sum off all timeout's and animations have to be less than this value
+
+
+            function displaySequenceToTurnCount() {
+                // console.log('logic array', logicArray);
+                var c = 0;
+                // timeout
+                var flashSeq = setInterval(function() {
+                    var activeCell = $('.cell').eq(logicArray[c]) // returns element with index
+                    activeCell.animate({
+                        opacity: '1'
+                    }, 100, function() { setTimeout(flashOn(activeCell)), 10 });
+                    c++;
+                    if (c == logicArray.length) {
+                        clearInterval(flashSeq);
+                    };
+
+                }, 600);
+                // How long before the next flash, sum off all timeout's and animations have to be less than this value
 
 
 
-    }
-    displaySequenceToTurnCount();
-    cells.on("click", userClick);
+            }
+            displaySequenceToTurnCount();
+            cells.on("click", userClick);
+        }) // END OF EASY MODE
+
+
+    // START OF MEDIUM MODE
+    $("#medium").one("click", function() {
+            $("#easy").hide();
+            $("#hard").hide();
+
+            var startEasy = function() {}
+                // Populate logic array with a random sequence
+            var fillLogic = function() {
+                var randomNumber = Math.floor(Math.random() * 4);
+                logicArray.push(randomNumber);
+            }
+
+
+            // Function to flash a cell back on
+            var flashOn = function(el1) {
+                $(el1).animate({
+                    opacity: '0.3'
+                }, 100);
+            }
+
+            // Check for wrong answer
+            var wrongAnswer = function() {
+                document.getElementById("wrongChime").play(); // play wrong chime
+                console.log("Chime: wrong");
+                $("body").css("background-color", "white");
+                window.setTimeout(function() {
+                    $("body").css("background-color", "black");
+                }, 100);
+            }
+
+            // SHOW CURRENT SCORE
+            var showScore = function() {
+                score++;
+                console.log("Score: " + score);
+                $(".scoreNumber").empty();
+                $(".scoreNumber").append(score)
+            }
+
+            // SHOW HIGH SCORE 
+            var showHighScore = function() {
+                var highScore = 0;
+                if (score > highScore) {
+                    highScore = score;
+                }
+                $(".highScoreNumber").empty();
+                $(".highScoreNumber").append(highScore);
+
+            }
+
+
+            var checkSeq = function() {
+                console.log('checking sequence')
+                var checkCounter = 0;
+                console.log('logic array is ' + logicArray);
+                console.log('user array is: ' + userArray)
+                for (var i = 0; i < logicArray.length; i++) {
+                    if (userArray[i] !== logicArray[i]) {
+                        console.log('WRONG')
+                        return wrongAnswer();
+                    } else {
+
+                    }
+                }
+                console.error('resetting userArray')
+                document.getElementById("rightChime").play(); // PLAY CORRECT CHIME!
+                userArray = [];
+                fillLogic();
+                displaySequenceToTurnCount();
+                showScore();
+                showHighScore();
+
+            }
+
+
+
+            // Flash cell on user click
+
+            var userClick = function() {
+                var activeCell = $("#" + event.target.id);
+                // console.log(activeCell);
+                activeCell.animate({ // animate click
+                    opacity: '1'
+                }, 100, function() { setTimeout(flashOn(activeCell)), 50 });
+                userArray.push(parseInt(event.target.id));
+
+                if (userArray.length == logicArray.length) {
+                    console.log('array lengths match');
+                    checkSeq();
+                }
+            };
+
+
+
+
+            function displaySequenceToTurnCount() {
+                // console.log('logic array', logicArray);
+                var c = 0;
+                // timeout
+                var flashSeq = setInterval(function() {
+                    var activeCell = $('.cell').eq(logicArray[c]) // returns element with index
+                    activeCell.animate({
+                        opacity: '1'
+                    }, 100, function() { setTimeout(flashOn(activeCell)), 10 });
+                    c++;
+                    if (c == logicArray.length) {
+                        clearInterval(flashSeq);
+                    };
+
+                }, 400);
+                // How long before the next flash, sum off all timeout's and animations have to be less than this value
+
+
+
+            }
+            displaySequenceToTurnCount();
+            cells.on("click", userClick);
+        }) // END OF MEDIUM MODE
+
+
+    // START OF HARD MODE
+    $("#hard").one("click", function() {
+            $("#easy").hide();
+            $("#medium").hide();
+
+            var startEasy = function() {}
+                // Populate logic array with a random sequence
+            var fillLogic = function() {
+                var randomNumber = Math.floor(Math.random() * 4);
+                logicArray.push(randomNumber);
+            }
+
+
+            // Function to flash a cell back on
+            var flashOn = function(el1) {
+                $(el1).animate({
+                    opacity: '0.3'
+                }, 100);
+            }
+
+            // Check for wrong answer
+            var wrongAnswer = function() {
+                document.getElementById("wrongChime").play(); // play wrong chime
+                console.log("Chime: wrong");
+                $("body").css("background-color", "white");
+                window.setTimeout(function() {
+                    $("body").css("background-color", "black");
+                }, 100);
+            }
+
+            // SHOW CURRENT SCORE
+            var showScore = function() {
+                score++;
+                console.log("Score: " + score);
+                $(".scoreNumber").empty();
+                $(".scoreNumber").append(score)
+            }
+
+            // SHOW HIGH SCORE 
+            var showHighScore = function() {
+                var highScore = 0;
+                if (score > highScore) {
+                    highScore = score;
+                }
+                $(".highScoreNumber").empty();
+                $(".highScoreNumber").append(highScore);
+
+            }
+
+
+            var checkSeq = function() {
+                console.log('checking sequence')
+                var checkCounter = 0;
+                console.log('logic array is ' + logicArray);
+                console.log('user array is: ' + userArray)
+                for (var i = 0; i < logicArray.length; i++) {
+                    if (userArray[i] !== logicArray[i]) {
+                        console.log('WRONG')
+                        return wrongAnswer();
+                    } else {
+
+                    }
+                }
+                console.error('resetting userArray')
+                document.getElementById("rightChime").play(); // PLAY CORRECT CHIME!
+                userArray = [];
+                fillLogic();
+                displaySequenceToTurnCount();
+                showScore();
+                showHighScore();
+            }
+
+
+
+            // Flash cell on user click
+
+            var userClick = function() {
+                var activeCell = $("#" + event.target.id);
+                // console.log(activeCell);
+                activeCell.animate({ // animate click
+                    opacity: '1'
+                }, 100, function() { setTimeout(flashOn(activeCell)), 50 });
+                userArray.push(parseInt(event.target.id));
+
+                if (userArray.length == logicArray.length) {
+                    console.log('array lengths match');
+                    checkSeq();
+                }
+            };
+
+
+
+
+            function displaySequenceToTurnCount() {
+                // console.log('logic array', logicArray);
+                var c = 0;
+                // timeout
+                var flashSeq = setInterval(function() {
+                    var activeCell = $('.cell').eq(logicArray[c]) // returns element with index
+                    activeCell.animate({
+                        opacity: '1'
+                    }, 100, function() { setTimeout(flashOn(activeCell)), 10 });
+                    c++;
+                    if (c == logicArray.length) {
+                        clearInterval(flashSeq);
+                    };
+
+                }, 200);
+                // How long before the next flash, sum off all timeout's and animations have to be less than this value
+
+
+
+            }
+            displaySequenceToTurnCount();
+            cells.on("click", userClick);
+        }) // END OF HARD MODE
 
 }); // jQuery End
